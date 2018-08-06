ecf5ff68a61f95a31bfaa17996a4c1cedda21563
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-08-06 16:02:15 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -25,7 +25,7 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-    info = phone.scan()
+    info = phone.scan(/(\d\d\d)-(\d\d\d)-(\d\d\d\d))/)
 end
 if info.nil?
   false
