4b94c15a044086b61d38953519beacc6e54c44ba
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-08-06 16:30:05 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -8,7 +8,7 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
- text.scan(/\b(un\w*ing)\b/)
+ text.scan(/\b(un\w*ing)\b/).flatten
 end
 
 def words_five_letters_long(text)
@@ -16,7 +16,7 @@ text.scan(/\b\w{5}\b/)
 end
 
 def five(text)
-text.scan(/\b\w{5}\b/)
+text.scan(/\b(un\w*ing)\b/)
 end
 text="unassuming ambiguous understanding pomp circumstance uninteresting uncompromising grouchy corollary"
 five(text)
