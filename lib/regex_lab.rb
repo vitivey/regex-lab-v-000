78be4045adbbc501c99e9f5f8c521209b189c976
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-08-06 15:28:41 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -3,15 +3,15 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  text.scan(^un.*ing$)
+  #text.scan(^un.*ing$)
 end
 
 def words_five_letters_long(text)
-  text.scan(\w{5})
+  #text.scan(\w{5})
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  text.match(\W.+\.)
+  #text.match(\W.+\.)
 end
 
 def valid_phone_number?(phone)
