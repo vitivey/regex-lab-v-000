30194ff88c4170c8d1086e976ebadb332db016ef
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-08-06 15:29:23 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,17 +1,17 @@
 def starts_with_a_vowel?(word)
-  word.scan(^[aeiou].+)
+  word.scan(/^([aeiou]).+/)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  #text.scan(^un.*ing$)
+  text.scan(/^(un.*)ing$/)
 end
 
 def words_five_letters_long(text)
-  #text.scan(\w{5})
+  text.scan(/\w{5}/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  #text.match(\W.+\.)
+  text.match(/\W.+\./)
 end
 
 def valid_phone_number?(phone)
