eb30589971f7ff9c9b0d6a1112bc588dd6cf93c8
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-08-06 16:01:14 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -8,7 +8,7 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
- text.scan(/(un)(\w)*ing/)
+ text.scan(/\b(un\w*ing)\b/)
 end
 
 def words_five_letters_long(text)
@@ -16,7 +16,7 @@ text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  info = text.match(/[A-Z].*\./)
+  info = text.match(/(A-Z].*\./)
   if info.nil?
     false
   else
@@ -25,5 +25,10 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-
+    info = phone.scan()
+end
+if info.nil?
+  false
+else
+  true
 end
