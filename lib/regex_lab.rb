5240cad6c60b293f77b7534f368af7edb076701d
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-08-06 16:34:54 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -26,7 +26,7 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-    info = phone.scan(/\d?\D?\d{3}\D*\d{3}\D?\d{4}/)
+    info = phone.scan(/^\(?\d{3}\)? ?\d{3} ?-?\d{4}/)
 if info.nil?
   false
 else
