94a2c3f2cd7b9bc9a1fc15ffcdcfd3719da8a976
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-08-06 15:45:48 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -8,7 +8,7 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  info = text.scan(/^(un.*)ing$/)
+  info = text.scan(/^(un.*)ing\s$/)
   if info.nil?
     false
   else
