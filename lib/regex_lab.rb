d813ed4f0f9232737a1f654ed7e3f22c5f7b6885
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-08-06 15:27:34 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,5 @@
 def starts_with_a_vowel?(word)
-  word.scan(^[aeiou].+)
+  word.scan(^([aeiou]).+)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
@@ -7,11 +7,11 @@ def words_starting_with_un_and_ending_with_ing(text)
 end
 
 def words_five_letters_long(text)
-  text.scan()
+  text.scan(\w{5})
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  text.match
+  text.match(\W.+\.)
 end
 
 def valid_phone_number?(phone)
